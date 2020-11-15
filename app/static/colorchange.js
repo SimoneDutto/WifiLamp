f32f3adffa6fef6100ab4e5110cad790ec9b591c
SimoneDutto <you@example.com>
2020-11-15 16:57:53 +0100

    Fix

--- app/static/colorchange.js
+++ app/static/colorchange.js
@@ -17,7 +17,7 @@ function setColor(){
         green: g_hex,
         blue: b_hex
       };
-      jQuery.getJSON("/changeColor=?", data, function (result) {
+      jQuery.getJSON("/changeColor.html", data, function (result) {
       //alert(result);
     });
   body.style.backgroundColor = hex; 
