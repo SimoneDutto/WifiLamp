17c931dd0434247d1b5eca7e82c02409a34b68c9
SimoneDutto <you@example.com>
2020-11-15 17:03:50 +0100

    FIx

--- app/routes.py
+++ app/routes.py
@@ -10,11 +10,11 @@ def index():
 
 @app.route('/changeColor', methods=["GET"])
 def changeColor():
-    redc = int(request.args.get("red", ""))
-    greenc = int(request.args.get("green", ""))
-    bluec = int(request.args.get("blue", ""))
+    redc = int(request.args.get("red", ""),16)
+    greenc = int(request.args.get("green", ""),16)
+    bluec = int(request.args.get("blue", ""),16)
     RGBColor(redc, greenc, bluec) # change color
-    return render_template('prova.html')
+    return 
 
 
 
